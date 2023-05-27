023d346037a1d3492c458fd35907ce3cab7b57f7
Ponlawat W <ponlawat_w@cmu.ac.th>
2023-05-27 15:56:37 +0700

    Bumped version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023043000;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2023052700;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016052300;        // Requires this Moodle version
 $plugin->component = 'block_forum_report'; // Full name of the plugin (used for diagnostics)
 
